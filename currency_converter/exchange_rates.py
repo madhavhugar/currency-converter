5f67c3624d7c480e4eb5a73e96d40b78be0cfe23
madhavhugar <madhav.hugar@gmail.com>
2020-05-25 21:34:37 +0200

    Include function documentation for exchange_rate module

--- currency_converter/exchange_rates.py
+++ currency_converter/exchange_rates.py
@@ -4,15 +4,19 @@ import xml.etree.ElementTree as et
 import pandas as pd
 
 
-# TODO: A better way to search the namespaced XML example is to create a
-# dictionary with your own prefixes and use those in the search functions
-
 def fetch_xml():
+    """
+    Fetch XML containing the euro exchange rate
+    """
     EURO_REF_XML = 'https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml'
     return urlopen(EURO_REF_XML)
 
 
 def parse_xml(xml):
+    """
+    Parse the XML and returns a list of dictionary objects
+    [{date, currency, rate}]
+    """
     eurofxref = '{http://www.ecb.int/vocabulary/2002-08-01/eurofxref}Cube'
     xtree = et.parse(xml)
     reference = []
@@ -30,24 +34,37 @@ def parse_xml(xml):
 
 
 def create_exchange_dataframe(reference, columns):
+    """
+    Parses list of dictionary objects and returns a
+    Dataframe with the specified columns
+    """
     return pd.DataFrame(reference, columns=columns)
 
 
 def load_exchange_data():
+    """
+    Returns a Dataframe containing the euro exchange rates
+    """
     xml = fetch_xml()
     reference = parse_xml(xml)
     df = create_exchange_dataframe(reference, ['date', 'rate', 'currency'])
     return df
 
 
-# TODO: Better way of caching the exchange rates
 def lookup_exchange_rate(date, currency):
+    """
+    Queries the exchange rate for a given date & currency
+    """
     rates_df = load_exchange_data()
     out = rates_df.query(f'date == "{date}" and currency == "{currency}"')
     return float(out.rate.values[0])
 
 
 def convert_amount(amount, date, src_currency, dest_currency):
+    """
+    Returns a converted amount for a given
+    amount, date, src_currency & dest_currency
+    """
     if src_currency == 'EUR' and dest_currency == 'EUR':
         return round(amount, 2)
     if src_currency == 'EUR' and dest_currency != 'EUR':
